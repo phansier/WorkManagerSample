0b9bdf59e3286115edabf3d701d44da334950a96
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-04-28 15:40:04 +0300

    Add notification

--- app/src/main/java/ru/beryukhov/wm_sample/workers/UpdateScanWorker.kt
+++ app/src/main/java/ru/beryukhov/wm_sample/workers/UpdateScanWorker.kt
@@ -1,6 +1,11 @@
 package ru.beryukhov.wm_sample.workers
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LiveData
 import androidx.work.*
 import androidx.work.ListenableWorker.Result
@@ -8,6 +13,8 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
+
+import ru.beryukhov.wm_sample.R
 
 const val PROGRESS = "PROGRESS"
 internal const val TAG_SCAN_PROGRESS = "tag_progress"
@@ -87,12 +94,51 @@ class ScanWorker(appContext: Context, workerParams: WorkerParameters) :
     RxWorker(appContext, workerParams) {
     override fun createWork(): Single<Result> {
         return Observable.interval(1, TimeUnit.SECONDS)
-            .take(100)
-            .doOnEach { setProgressAsync(workDataOf(PROGRESS to it.value)) }
+            .take(10)
+            .map { it * 10 }
+            .doOnEach {
+                setProgressAsync(workDataOf(PROGRESS to it.value))
+                setForegroundAsync(createForegroundInfo("In progress... ${it.value} %"))
+            }
             .collectInto(Result.success()) { result, _ -> result }
             .subscribeOn(Schedulers.computation())
     }
+
+    private fun createForegroundInfo(progress: String): ForegroundInfo {
+        val id = applicationContext.getString(R.string.notification_channel_id)
+        val title = applicationContext.getString(R.string.notification_title)
+        val cancel = applicationContext.getString(R.string.cancel_download)
+        // This PendingIntent can be used to cancel the worker
+        val intent = WorkManager.getInstance(applicationContext)
+            .createCancelPendingIntent(getId())
+
+        // Create a Notification channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel(id)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, id)
+            .setContentTitle(title)
+            .setTicker(title)
+            .setContentText(progress)
+            .setSmallIcon(android.R.drawable.ic_menu_search)
+            .setOngoing(true)
+            // Add the cancel action to the notification which can
+            // be used to cancel the worker
+            .addAction(android.R.drawable.ic_delete, cancel, intent)
+            .build()
+
+        return ForegroundInfo(42,notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel(id: String) {
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(NotificationChannel(id, "Scan notifications", NotificationManager.IMPORTANCE_HIGH))
+    }
 }
+
+
 
 /**
  * Fakes
